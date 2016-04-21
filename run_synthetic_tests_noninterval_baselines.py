e5d6b5c99484a9368e903308fedcf1375ae5dbee
Björn Barz <bjoern.barz@uni-jena.de>
2016-04-21 09:24:15 +0200

    AP for non-interval baselines

--- run_synthetic_tests_noninterval_baselines.py
+++ run_synthetic_tests_noninterval_baselines.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
-import maxdiv
-import maxdiv_tools
-import preproc
+import maxdiv, maxdiv_tools, preproc, eval
 import argparse
 import sklearn
 import sklearn.metrics
@@ -17,9 +15,37 @@ except ImportError:
     # pickle if available.
     import pickle
 
+
+def pointwiseScoresToIntervals(scores, min_length = 0):
+    
+    sorted_scores = sorted(scores)
+    first_th = sorted_scores[int(len(scores) * 0.7)]
+    max_score = sorted_scores[-1]
+    
+    thresholds = np.linspace(first_th, max_score, 10, endpoint = False)
+    scores = np.array(scores)
+    regions = []
+    for th in thresholds[::-1]:
+        regions += [(a, b, scores[a:b].min()) for a, b in eval.pointwiseLabelsToIntervals(scores >= th) if b - a >= min_length]
+    
+    # Non-maxima suppression
+    include = np.ones(len(regions), dtype = bool) # suppressed intervals will be set to False
+    for i in range(len(regions)):
+        if include[i]:
+            a, b, score = regions[i]
+            # Exclude intervals with a lower score overlapping this one
+            for j in range(i + 1, len(regions)):
+                if include[j] and (maxdiv.IoU(a, b - a, regions[j][0], regions[j][1] - regions[j][0]) > 0.5):
+                    include[j] = False
+    
+    return [r for i, r in enumerate(regions) if include[i]]
+
+
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--novis', action='store_true', help='skip the visualization')
 parser.add_argument('--extremetypes', help='types of extremes to be tested', nargs='+',default=[])
+parser.add_argument('--preproc', help='preprocessing method', choices=[None,'td'],default=None)
+parser.add_argument('--extint_min_len', help='minimum length of the extreme interval', default=10, type=int)
 
 args = parser.parse_args()
 
@@ -31,7 +57,7 @@ with open('testcube.pickle', 'rb') as fin:
 extremetypes = set(args.extremetypes)
 
 aucs = {}
-num = 0
+aps = {}
 for ftype in f:
     print ('-- {} --'.format(ftype))
     if len(extremetypes)>0 and not ftype in extremetypes:
@@ -39,18 +65,31 @@ for ftype in f:
 
     funcs = f[ftype]
     ygts = y[ftype]
+    regions = []
     aucs[ftype] = []
     for i in range(len(funcs)):
-        func = funcs[i]
+        if args.preproc == 'td':
+            func = preproc.td(funcs[i])
+        else:
+            func = funcs[i]
         ygt = ygts[i]
         
         scores = hotellings_t(func)
+        regions.append(pointwiseScoresToIntervals(scores, args.extint_min_len))
+        if not args.novis:
+            eval.plotDetections(funcs[i], regions[-1], ygt, silent = False)
         
         fpr, tpr, thresholds = sklearn.metrics.roc_curve(ygt, scores, pos_label=1)
         auc = sklearn.metrics.auc(fpr, tpr)
         aucs[ftype].append(auc)
         print ("AUC: {}".format(auc))
+    
+    aps[ftype] = eval.average_precision(ygts, regions)
 
-print('-- Aggregated Results --')
+print('-- Aggregated AUC --')
 for ftype in aucs:
     print ("{}: {} (+/- {})".format(ftype, np.mean(aucs[ftype]), np.std(aucs[ftype])))
+
+print('-- Average Precision --')
+for ftype in aps:
+    print ("{}: {}".format(ftype, aps[ftype]))
