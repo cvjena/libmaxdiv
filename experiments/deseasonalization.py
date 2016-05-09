Normalization and minor fixes for Python 3

experiments/deseasonalization.py
@@ -3,7 +3,14 @@ sys.path.append('..')
 
 import numpy as np
 import matplotlib.pyplot as plt
+from maxdiv import maxdiv_util
 import datasets
+
+
+def sample_gp(X, meany, sigma, n=1, noise=0.001):
+    """ sample a function from a Gaussian process with Gaussian kernel """
+    K = maxdiv_util.calc_gaussian_kernel(X, sigma) + noise * np.eye(X.shape[1])
+    return np.random.multivariate_normal(meany, K, n)
 
 
 # Load some time series with seasonal patterns
@@ -46,7 +53,11 @@ for i in range(6):
     print('Period of {}. right singular vector: {} -> {}'.format(i+1, period, float(minSVDLen) / period))
 plt.show()
 # Remove some leading singular values
-rsRem = int(raw_input('Enter number of right-singular vectors to remove: '))
+try:
+    rsRem = raw_input('Enter number of right-singular vectors to remove: ')
+except:
+    rsRem = input('Enter number of right-singular vectors to remove: ')
+rsRem = int(rsRem)
 mm_s[:rsRem] = 0.0
 mm_norm = mm_u.dot(np.diag(mm_s).dot(mm_vt))
 
@@ -91,3 +102,30 @@ for id in ids:
             ax.plot([0, len(func)-1], [th, th], '--r')
             ax.plot(period_ind, ps[period], 'r.')
     plt.show()
+
+
+# Synthetic multivariate example: Seasonal correlations
+print('-- Multivariate --')
+mv_period = 20
+np.random.seed(0)
+X = np.linspace(0.0, 1.0, 250)
+X = np.reshape(X, [1, len(X)])
+gp = sample_gp(X, np.zeros(X.shape[1]), 0.01)
+func = np.vstack((gp, gp * np.sin(X * mv_period)))
+func += np.random.randn(*func.shape) * 0.1
+
+# Normalize by multivariate Z Score
+norm_func = func.copy()
+for h in range(mv_period):
+    hourly_values = func[:,h::mv_period]
+    mu = np.mean(hourly_values, axis = 1)
+    cov = np.cov(hourly_values)
+    zeromean_X = (hourly_values.T - mu).T
+    norm_func[:,h::mv_period] = np.dot(np.linalg.inv(cov), zeromean_X)
+
+fig = plt.figure()
+ax = fig.add_subplot(211, title = 'Multivariate TS with seasonal correlations')
+ax.plot(func.T)
+ax = fig.add_subplot(212, title = 'De-seasonalized by Z Score')
+ax.plot(norm_func.T)
+plt.show()

maxdiv/baselines_noninterval.py
@@ -1,6 +1,6 @@
 import numpy as np
-from eval import pointwiseLabelsToIntervals
-from maxdiv_util import calc_gaussian_kernel, IoU
+from .eval import pointwiseLabelsToIntervals
+from .maxdiv_util import calc_gaussian_kernel, IoU
 
 
 def hotellings_t(X):
@@ -21,7 +21,10 @@ def hotellings_t(X):
 
 
 def pointwiseKDE(X, kernel_sigma_sq = 1.0):
-    """ Scores every point in the time series by Kernel Density Estimation. """
+    """ Scores every point in the time series by Kernel Density Estimation.
+    
+    Note: The values of X should be in range [-1,1] to avoid numeric problems.
+    """
     
     # Compute kernel matrix
     K = calc_gaussian_kernel(X, kernel_sigma_sq, False)
@@ -105,6 +108,8 @@ def pointwiseRegionProposals(func, extint_min_len = 20, extint_max_len = 150,
         score_mean = np.mean(scores)
         score_sd = np.std(scores)
     score_max = np.max(scores)
+    if score_max <= 1e-16:
+        return
     th = score_mean + sd_th * score_sd
     while not np.any(scores >= th):
         sd_th *= 0.8

maxdiv/maxdiv.py
@@ -18,8 +18,8 @@ from numpy.linalg import slogdet, inv, solve
 from scipy.linalg import cholesky, solve_triangular
 from scipy.stats import multivariate_normal
 import math, time, types
-import maxdiv_util, preproc
-from baselines_noninterval import pointwiseRegionProposals
+from . import maxdiv_util, preproc
+from .baselines_noninterval import pointwiseRegionProposals
 
 def get_available_methods():
     return ['parzen', 'gaussian_cov', 'gaussian_id_cov', 'gaussian_global_cov']
@@ -430,12 +430,16 @@ def find_max_regions(intervals, num_intervals = None, overlap_th = 0.0):
 def maxdiv(X, method = 'parzen', num_intervals = 1, proposals = 'dense', **kwargs):
     """ Wrapper function for calling maximum divergent regions """
     if 'preproc' in kwargs:
-        if kwargs['preproc']=='local_linear':
-            X = preproc.local_linear_regression(X)
-        elif kwargs['preproc']=='td':
-            X = preproc.td(X)
-        elif not kwargs['preproc'] is None:
-            raise Exception("Unknown preprocessing method {}".format(kwargs['preproc']))
+        preprocs = kwargs['preproc'] if isinstance(kwargs['preproc'], list) or isinstance(kwargs['preproc'], tuple) else [kwargs['preproc']]
+        for prep in preprocs:
+            if prep == 'normalize':
+                X = preproc.normalize_time_series(X)
+            elif prep == 'local_linear':
+                X = preproc.local_linear_regression(X)
+            elif prep == 'td':
+                X = preproc.td(X)
+            elif not prep is None:
+                raise Exception("Unknown preprocessing method {}".format(prep))
         del kwargs['preproc']
     
     if 'proposalparameters' in kwargs:

maxdiv/preproc.py
@@ -44,5 +44,5 @@ def normalize_time_series(ts):
     """ Normalizes each dimension of a time series by subtracting the mean and dividing by the maximum. """
     
     ts = (ts.T - ts.mean(axis = 1)).T
-    ts = (ts.T / ts.max(axis = 1)).T
+    ts = (ts.T / np.abs(ts).max(axis = 1)).T
     return ts