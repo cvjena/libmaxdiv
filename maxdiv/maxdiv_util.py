Fixed & disabled normalization of gaussian kernel
It is irrelevant for use with the KL divergence.

maxdiv/maxdiv_util.py
@@ -23,7 +23,7 @@ def calc_gaussian_kernel(X, kernel_sigma_sq = 1.0, normalized=True):
     # compute proper normalized Gaussian kernel values
     K = np.exp(-D/(2.0*kernel_sigma_sq))
     if normalized:
-        K = K / np.sqrt(2*np.pi*kernel_sigma_sq)
+        K = K / ((2*np.pi*kernel_sigma_sq) ** (X.shape[0] / 2))
     return K
 
 