16174ca4fe25508860f7eb3c56a299da29191af7
Björn Barz <bjoern.barz@uni-jena.de>
2016-07-15 16:59:53 +0200

    Fixes for CoastDat utilities

--- experiments/maxdiv_coastdat_wrapper.py
+++ experiments/maxdiv_coastdat_wrapper.py
@@ -18,11 +18,11 @@ coastdat_params_p = POINTER(coastdat_params_t)
 c_uint_p = POINTER(c_uint)
 
 
-libcoastdat = CDLL('maxdiv_coastdat.so')
+libcoastdat = CDLL('./maxdiv_coastdat.so')
 
 maxdiv_coastdat = CFUNCTYPE(c_int, maxdiv_params_p, coastdat_params_p, detection_p, c_uint_p)(
                             ('maxdiv_coastdat', libcoastdat), ((1, 'params'), (1, 'data_params'), (1, 'detection_buf'), (1, 'detection_buf_size')))
 
-maxdiv_coastdat_context_window_size = CFUNCTYPE(c_int, maxdiv_params_p)(('maxdiv_coastdat_context_window_size', libcoastdat), ((1, 'data_params'),))
+maxdiv_coastdat_context_window_size = CFUNCTYPE(c_int, coastdat_params_p)(('maxdiv_coastdat_context_window_size', libcoastdat), ((1, 'data_params'),))
 
-maxdiv_coastdat_default_params = CFUNCTYPE(c_void_p, maxdiv_params_p)(('maxdiv_coastdat_default_params', libcoastdat), ((1, 'data_params'),))
+maxdiv_coastdat_default_params = CFUNCTYPE(c_void_p, coastdat_params_p)(('maxdiv_coastdat_default_params', libcoastdat), ((1, 'data_params'),))
