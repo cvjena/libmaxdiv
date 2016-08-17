50f3d484a5454753748a1fcc18d71dea36cd32dd
Björn Barz <bjoern.barz@uni-jena.de>
2016-08-17 16:53:09 +0200

    Adjusted anomaly ratio experiment

--- tools/test_anomaly_ratio.py
+++ tools/test_anomaly_ratio.py
@@ -26,10 +26,11 @@ def sample_gp_with_meanshift(length, anomaly_length, sigma = 5.0, n=1, noise=0.0
 
 
 # Set up CLI argument parser
+AVAIL_METHODS = maxdiv.get_available_methods() + ['gaussian_ts']
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--methods', help='maxdiv method', choices=maxdiv.get_available_methods(), nargs='+', default=[])
+parser.add_argument('--methods', help='maxdiv method', choices=AVAIL_METHODS, nargs='+', default=[])
 parser.add_argument('--preproc', help='use a pre-processing method', default='td', choices=preproc.get_available_methods())
-parser.add_argument('--mode', help='Mode for KL divergence computation', choices=['OMEGA_I', 'SYM', 'I_OMEGA', 'LAMBDA', 'IS_I_OMEGA'], default='I_OMEGA')
+parser.add_argument('--mode', help='Mode for KL divergence computation', choices=['OMEGA_I', 'SYM', 'I_OMEGA', 'LAMBDA', 'IS_I_OMEGA', 'TS'], default='I_OMEGA')
 parser.add_argument('--kernel_sigma_sq', help='kernel sigma square hyperparameter for Parzen estimation', type=float, default=1.0)
 parser.add_argument('--alpha', help='Hyperparameter for the KL divergence', type=float, default=1.0)
 
@@ -37,18 +38,20 @@ args = parser.parse_args()
 args_dict = vars(args)
 parameters = {parameter_name: args_dict[parameter_name] for parameter_name in ('preproc', 'mode', 'alpha')}
 
-methods = set(args.methods) & set(maxdiv.get_available_methods())
+methods = set(args.methods) & set(AVAIL_METHODS)
 if len(methods) == 0:
-    methods = maxdiv.get_available_methods()
+    methods = AVAIL_METHODS
 
-n = 250 # length of time series
+n = 500 # length of time series
 m = 20 # number of time series per anomaly length
 
 # Measure performance with various lengths of anomalies
 aucs = { method : [] for method in methods }
 aps = { method : [] for method in methods }
 ratios = []
-for l in range(15, 121, 15):
+for l in range(20, 201, 15):
+    
+    print(l)
     
     funcs = [sample_gp_with_meanshift(n, l) for i in range(m)]
     
@@ -58,7 +61,7 @@ for l in range(15, 121, 15):
         
         for i in range(m):
             gp, ygt = funcs[i]
-            regions.append(maxdiv.maxdiv(gp, method = method, num_intervals = 5, extint_min_len = 10, extint_max_len = 120,
+            regions.append(maxdiv.maxdiv(gp, method = method, num_intervals = 5, extint_min_len = 20, extint_max_len = 220,
                                          kernelparameters={'kernel_sigma_sq': args.kernel_sigma_sq}, **parameters))
             auc.append(eval.auc(ygt, regions[-1], n))
         
@@ -75,8 +78,8 @@ sp_ap = fig_ap.add_subplot(111, xlabel = 'Length of anomaly / Length of time ser
 for method in methods:
     sp_auc.plot(ratios, aucs[method], marker = 'x', label = method)
     sp_ap.plot(ratios, aps[method], marker = 'x', label = method)
-sp_auc.legend(loc = 'lower right')
-sp_ap.legend(loc = 'lower right')
+sp_auc.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=len(methods), mode="expand", borderaxespad=0.)
+sp_ap.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=len(methods), mode="expand", borderaxespad=0.)
 fig_auc.savefig('anomaly_ratio_auc.svg')
 fig_ap.savefig('anomaly_ratio_ap.svg')
 plt.show()
