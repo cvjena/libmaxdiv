2243b080b2409df309d3c9e93f102932ec6652d4
Björn Barz <bjoern.barz@uni-jena.de>
2016-09-11 14:25:58 +0200

    Updated timing experiment

--- experiments/timing_multvar.py
+++ experiments/timing_multvar.py
@@ -24,77 +24,70 @@ if (len(sys.argv) < 2) or (sys.argv[1].lower() == 'noplot'):
     # Parameters
     min_len = 10
     max_len = 100
-    N = np.arange(200, 2501, 100)
-    times = np.ndarray((len(N), 4), dtype = np.float64)
+    N = 1000
+    dims = np.arange(1, 50)
+    times = np.ndarray((len(dims), 3), dtype = np.float64)
 
     # Prepare libmaxdiv pipelines
     params = libmaxdiv_wrapper.maxdiv_params_t()
     libmaxdiv_wrapper.libmaxdiv.maxdiv_init_params(params)
     params.min_size[0] = min_len
     params.max_size[0] = max_len
-    params.preproc.embedding.kt = 3
-    params.preproc.embedding.temporal_borders = libmaxdiv_wrapper.enums['MAXDIV_BORDER_POLICY_CONSTANT']
 
     params.estimator = libmaxdiv_wrapper.enums['MAXDIV_GAUSSIAN']
     pipeline_gaussian = libmaxdiv_wrapper.libmaxdiv.maxdiv_compile_pipeline(params)
 
     params.estimator = libmaxdiv_wrapper.enums['MAXDIV_KDE']
     pipeline_parzen = libmaxdiv_wrapper.libmaxdiv.maxdiv_compile_pipeline(params)
+    
+    params.estimator = libmaxdiv_wrapper.enums['MAXDIV_ERPH']
+    pipeline_erph = libmaxdiv_wrapper.libmaxdiv.maxdiv_compile_pipeline(params)
 
     # Measure runtimes and write them to timing.csv
-    with open('timing.csv', 'w') as outFile:
-        outFile.write('Length,Gaussian (Python),KDE (Python),Gaussian (libmaxdiv),KDE (libmaxdiv)\n')
+    with open('timing_multvar.csv', 'w') as outFile:
+        outFile.write('Dimensions,Gaussian,KDE,ERPH\n')
         
-        for i, n in enumerate(N):
-            gps = sample_gp(n)
-            
-            start = time()
-            maxdiv.maxdiv(gps, 'gaussian_cov', None, 'dense', useLibMaxDiv = False, mode = 'I_OMEGA', preproc = 'td', extint_min_len = min_len, extint_max_len = max_len)
-            stop = time()
-            times[i, 0] = stop - start
-            
-            start = time()
-            maxdiv.maxdiv(gps, 'parzen', None, 'dense', useLibMaxDiv = False, mode = 'I_OMEGA', preproc = 'td', extint_min_len = min_len, extint_max_len = max_len)
-            stop = time()
-            times[i, 1] = stop - start
+        for i, dim in enumerate(dims):
+            gps = sample_gp(N, dim)
             
             start = time()
             libmaxdiv_wrapper.maxdiv_exec(gps, pipeline_gaussian, None)
             stop = time()
-            times[i, 2] = stop - start
+            times[i, 0] = stop - start
             
             start = time()
             libmaxdiv_wrapper.maxdiv_exec(gps, pipeline_parzen, None)
             stop = time()
-            times[i, 3] = stop - start
+            times[i, 1] = stop - start
             
-            outFile.write('{},{:.3f},{:.3f},{:.3f},{:.3f}\n'.format(n, *times[i,:]))
+            start = time()
+            libmaxdiv_wrapper.maxdiv_exec(gps, pipeline_erph, None)
+            stop = time()
+            times[i, 2] = stop - start
+            
+            outFile.write('{},{:.3f},{:.3f},{:.3f}\n'.format(dim, *times[i,:]))
             outFile.flush()
 
 else:
 
-    N = []
+    dims = []
     times = []
     with open(sys.argv[1]) as inFile:
         for i, d in enumerate(csv.reader(inFile)):
             if i > 0:
-                N.append(int(d[0]))
-                times.append([float(x) for x in d[1:5]])
+                dims.append(int(d[0]))
+                times.append([float(x) for x in d[1:4]])
     times = np.array(times)
 
 
 # Plot results
 if (len(sys.argv) < 2) or (sys.argv[1].lower() != 'noplot'):
     import matplotlib.pylab as plt
-    plt.plot(N, times[:, 0] * 1000, 'b--', label = 'Gaussian (Python)')
-    plt.plot(N, times[:, 1] * 1000, 'r--', label = 'KDE (Python)')
-    plt.plot(N, times[:, 2] * 1000, 'b-',  label = 'Gaussian (libmaxdiv)')
-    plt.plot(N, times[:, 3] * 1000, 'r-',  label = 'KDE (libmaxdiv)')
-    plt.xlabel('Length of Time Series')
-    plt.ylabel('Algorithm Run-Time')
-    plt.yscale('log')
-    ticks = [10 ** e for e in range(1, 6, 1)]
-    plt.yticks(ticks, ['{:.0f} s'.format(l / 1000) if l >= 1000 else '{:.0f} ms'.format(l) for l in ticks])
+    plt.plot(N, times[:, 0] * 1000, 'b-', label = 'Gaussian')
+    plt.plot(N, times[:, 1] * 1000, 'r-', label = 'KDE')
+    plt.plot(N, times[:, 2] * 1000, 'g-', label = 'ERPH')
+    plt.xlabel('Dimensionality of Time Series')
+    plt.ylabel('Algorithm Run-Time in Seconds')
     plt.grid(True)
     plt.legend(loc = 'lower right')
     plt.show()
