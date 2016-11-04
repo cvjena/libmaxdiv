0e3af64a305b1a400988aaa7c6476f7c389d9801
Erik Rodner <Erik.Rodner@uni-jena.de>
2016-11-04 09:31:31 +0100

    generalizing the spatial maxdiv interface (command arguments, etc.)

--- experiments/SLP/detect_slp.py
+++ experiments/SLP/detect_slp.py
@@ -3,6 +3,48 @@
 import os.path
 from utils import *
 from maxdiv.libmaxdiv_wrapper import *
+import argparse
+
+default_csv = '../coastdat/historic_storms.csv'
+default_gridspec = getSLPGridSpec()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--matfile', help='MAT file containing the tensor', default='SLP_ATL.mat')
+parser.add_argument('--tensorvar', help='Variable in the MAT containing the tensor', default='pres')
+parser.add_argument('--eventscsv', help='CSV file containing historic events', default=default_csv)
+parser.add_argument('--yearoffset', help='Grid specification: year offset', default=default_gridspec['year_offs'])
+parser.add_argument('--latoffset', help='Grid specification: latitude offset', default=default_gridspec['lat_offs'])
+parser.add_argument('--latstep', help='Grid specification: latitude step', default=default_gridspec['lat_step'])
+parser.add_argument('--lonstep', help='Grid specification: longitude step', default=default_gridspec['lon_step'])
+parser.add_argument('--lonoffset', help='Grid specification: longitude offset', default=default_gridspec['lon_offs'])
+parser.add_argument('--gridspecfile', help='Load grid specification from a simple text file: <var> = <value>')
+args = parser.parse_args()
+
+if args.gridspecfile is None:
+    gridspec = {
+            'year_offs': args.yearoffset,
+            'lat_offs': args.latoffset,
+            'lat_step': args.latstep,
+            'lon_step': args.lonstep,
+            'lon_offs': args.lonoffset,
+        }
+else:
+    print ("loading grid specifications from file {} ...".format(args.gridspecfile))
+    gridspec = {}
+    with open(args.gridspecfile, 'r') as gsf:
+        for line in gsf:
+            l = line.rstrip().lower()
+            variable, value = l.split(' = ')
+            gridspec[variable] = value
+    # check whether we have all necessary variables
+    # also set in the default
+    for k in default_gridspec:
+        if not k in gridspec:
+            sys.exit('Variable {} not specified in {}'.format(k, args.gridspecfile))
+        
+
+if args.eventscsv is None:
+    print ("No historic events specified, use {} for the SLP data.".format(default_csv))
 
 params = maxdiv_params_t()
 libmaxdiv.maxdiv_init_params(params)
@@ -15,6 +57,14 @@ params.preproc.embedding.kt = 3
 params.preproc.embedding.kx = params.preproc.embedding.ky = 1
 params.preproc.normalization = enums['MAXDIV_NORMALIZE_MAX']
 
-detections = maxdiv_exec(loadSLP(), params, 20)
+if args.eventscsv is None:
+    historic_events = []
+else:
+    historic_events = loadHistoricEvents(args.eventscsv)
 
-printDetections(detections)
+print ("loading tensor from {} ...".format(args.matfile))
+tensor = loadTensor(args.matfile, args.tensorvar)
+print ("running maxdiv ...")
+detections = maxdiv_exec(tensor, params, 20)
+print ("matching with historic events ...")
+printDetections(detections, gridspec, historic_events)
